23d3ac5f25b6aad700b3af39eed5f0671ae93344
欧舟 <wb-oz389525@alibaba-inc.com>
2020-01-14 18:28:50 +0800

    fix

--- push.sh
+++ push.sh
@@ -1,4 +1,12 @@
-ffmpeg \
+# bash push.sh 22222222 102 rtp_port rtcp_port
+# 前面两个写死在代码里面了
+
+ssrc=$1
+payload_type=$2
+rtp_port=$3
+rtcp_port=$4
+
+echo ffmpeg \
   -re \
   -v info \
   -stream_loop -1 \
@@ -8,4 +16,4 @@ ffmpeg \
   -map 0:v:0 \
   -pix_fmt yuv420p -c:v libvpx -b:v 1000k -deadline realtime -cpu-used 4 \
   -f tee \
-  "[select=v:f=rtp:ssrc=22222222:payload_type=102]rtp://127.0.0.1:10091?rtcpport=10058"
+  "[select=v:f=rtp:ssrc=$ssrc:payload_type=$payload_type]rtp://127.0.0.1:$rtp_port?rtcpport=$rtcp_port"
