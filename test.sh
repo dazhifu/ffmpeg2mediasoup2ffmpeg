1d8ae09d5dafce86ffcf338472daec5af27cf560
欧舟 <wb-oz389525@alibaba-inc.com>
2020-01-15 10:45:19 +0800

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-ffmpeg -re -i video.mp4 -vcodec copy -f rtp rtp://127.0.0.1:12345>s.sdp
+ffmpeg -re -i invite.mp4 -vcodec copy -an -f rtp rtp://127.0.0.1:11111 -vn -acodec copy -f rtp rtp://127.0.0.1:11112>s.sdp
+
+
 ffplay -protocol_whitelist "file,http,https,rtp,udp,tcp,tls" s.sdp
 
 
